8c111025b7186ce8a77cb1f066408542bef15bab
Santiago Squarzon <squarzon.santiago@gmail.com>
2024-06-20 13:00:09 -0300

    dev on module helper class.

--- tools/ProjectBuilder/ModuleHelper.cs
+++ tools/ProjectBuilder/ModuleHelper.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Management.Automation;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -15,25 +16,37 @@ public record struct ModuleDownload(string Module, Version Version)
         new(entry.Key.ToString(), LanguagePrimitives.ConvertTo<Version>(entry.Value));
 };
 
+internal record struct ModuleDownloadReference(string Zip, string Path);
+
 public sealed class ModuleHelper
 {
     public string ModulePath { get; }
 
-    private UriBuilder _builder = new(_base);
+    private readonly UriBuilder _builder = new(_base);
 
     private const string _base = "https://www.powershellgallery.com";
 
     private const string _path = "api/v2/package/{0}/{1}";
 
+    private List<Task<ModuleDownloadReference>>? _tasks;
+
+    private List<ModuleDownloadReference>? _completedTasks;
+
     public ModuleHelper(string path)
     {
         ModulePath = Path.Combine(path, "tools", "Modules");
+        if (!Directory.Exists(ModulePath))
+        {
+            Directory.CreateDirectory(ModulePath);
+        }
     }
 
     public string[] DownloadModules(ModuleDownload[] modules)
     {
-        List<Task<string>> tasks = new(modules.Length);
-        List<string> completed = new(modules.Length);
+        _tasks ??= new(modules.Length);
+        _completedTasks ??= new(modules.Length);
+        List<string> output = new(modules.Length);
+
         foreach ((string module, Version version) in modules)
         {
             string destination = GetDestination(module);
@@ -41,44 +54,42 @@ public sealed class ModuleHelper
             if (Directory.Exists(modulePath))
             {
                 Console.WriteLine($"Module '{module}' already downloaded. Skipping.");
-                completed.Add(modulePath);
+                output.Add(modulePath);
                 continue;
             }
 
             Console.WriteLine($"Installing build pre-req '{module}'");
             _builder.Path = string.Format(_path, module, version);
-            Task<string> task = DownloadAsync(_builder.Uri.ToString(), destination);
-            tasks.Add(task);
+            Task<ModuleDownloadReference> task = DownloadAsync(
+                uri: _builder.Uri.ToString(),
+                destination: destination,
+                expandPath: modulePath);
+            _tasks.Add(task);
         }
 
-        completed.AddRange(WaitTaskAsync(tasks).GetAwaiter().GetResult());
-        ExpandArchives([.. completed]);
-        return [.. completed];
+        WaitTaskAsync().GetAwaiter().GetResult();
+        ExpandArchives([.. _completedTasks]);
+        output.AddRange(_completedTasks.Select(e => e.Path));
+        return [.. output];
     }
 
-    private void ExpandArchives(string[] archives)
+    private static void ExpandArchives(ModuleDownloadReference[] archives)
     {
-        foreach (string zip in archives)
+        foreach ((string zip, string expandPath) in archives)
         {
-            ZipFile.ExtractToDirectory(
-                zip,
-                Path.GetFileNameWithoutExtension(zip));
+            ZipFile.ExtractToDirectory(zip, expandPath);
         }
     }
 
-    private async Task<string[]> WaitTaskAsync(
-        List<Task<string>> tasks)
+    private async Task WaitTaskAsync()
     {
-        List<string> completed = new(tasks.Count);
-        while (tasks.Count > 0)
+        while (_tasks?.Count > 0)
         {
-            Task<string> awaiter = await Task.WhenAny(tasks);
-            tasks.Remove(awaiter);
-            string module = await awaiter;
-            completed.Add(module);
+            Task<ModuleDownloadReference> awaiter = await Task.WhenAny(_tasks);
+            _tasks.Remove(awaiter);
+            ModuleDownloadReference module = await awaiter;
+            _completedTasks?.Add(module);
         }
-
-        return [.. completed];
     }
 
     private string GetDestination(string module) =>
@@ -87,12 +98,15 @@ public sealed class ModuleHelper
     private string GetModulePath(string module) =>
         Path.Combine(ModulePath, module);
 
-    private async Task<string> DownloadAsync(string uri, string destination)
+    private static async Task<ModuleDownloadReference> DownloadAsync(
+        string uri,
+        string destination,
+        string expandPath)
     {
         using FileStream fs = File.Create(destination);
         using HttpClient client = new();
         using Stream stream = await client.GetStreamAsync(uri);
         await stream.CopyToAsync(fs);
-        return destination;
+        return new ModuleDownloadReference(destination, expandPath);
     }
 }
