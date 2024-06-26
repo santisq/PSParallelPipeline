3036c4b92ba6d5c979e3fef16050ed4ee7f10495
Santiago Squarzon <squarzon.santiago@gmail.com>
2024-06-26 15:23:09 -0300

    increment wait time for pwsh 5.1 test

--- tests/PSParallelPipeline.tests.ps1
+++ tests/PSParallelPipeline.tests.ps1
@@ -150,7 +150,7 @@ Describe PSParallelPipeline {
                 } | Should -Throw -ExceptionType ([TimeoutException])
                 $timer.Stop()
                 $timer.Elapsed | Should -BeLessOrEqual ([timespan]::FromSeconds(2.2))
-                Start-Sleep 1
+                Start-Sleep 5
             }
         }
     }
